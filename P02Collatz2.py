59a118adb282db5b8299dd30bcd2d48463675822
user <qkrdmstjs0709@g.shingu.ac.kr>
2025-04-15 10:37:26 +0900

    리스트와넘파이로통계량구하기 함수도입

--- P02Collatz2.py
+++ P02Collatz2.py
@@ -1,61 +1,53 @@
-# 2025.4.2 파이썬수업 프로젝트 두번째
-# 콜라츠 추측, 또는 우박수
-# 1부터 1000까지숫자중, 가장많은 단계를 거쳐서 1로가는 수는 무엇인가?, 가장많은 단계는 몇단계인가
-# 규칙: n이 짝수 -> n/2
-#      n이 홀수 -> 3 * n + 1
-#  예: 5 -> 16 -> 8 -> 4 -> 2 -> 1   (5단계)
 import numpy as np
 import statistics
 import time
+from collections import Counter
 
-from P02Collatzfunc import collatz
+# collatz 함수 직접 정의
+def collatz(n):
+    count = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        count += 1
+    return count
 
 MAXNUM = 100
 
-# 2025.4.9. 우박수프로젝트 두번째: 기본 통계량 구하기
-# numpy 배열, list 두가지 방식으로 시험
-# 구하는 통계량: 최대값 평균, 중앙값, 표준편차, 최빈값,
-
-
-
-# 리스트방식
-
+# 리스트 방식
 start = time.time()
 ncountl = []
 
-for n in range(1,MAXNUM):
+for n in range(1, MAXNUM):
     ncount = collatz(n)
     ncountl.append(ncount)
 
-
-# print(ncountl)
-# print(sum(ncountl) / len(ncountl))
-
-# 최대값, 평균, 중앙값, 표준편차, 최빈값,
+print(f'[리스트 방식]')
 print(f'최대값={max(ncountl)}')
 print(f'평균={statistics.mean(ncountl):.5f}')
 print(f'중앙값={statistics.median(ncountl)}')
-# print(f'최빈값 = {statistics.mode(ncountl):.5f}')
-print(f'표준편차 ={statistics.stdev(ncountl):.5f}')
-
+print(f'표준편차={statistics.stdev(ncountl):.5f}')
+mode_val = Counter(ncountl).most_common(1)[0][0]
+print(f'최빈값={mode_val}')
 end = time.time()
-print(f'{end - start:.5f} 초가 걸렸습니다')
-
-
+print(f'{end - start:.5f} 초가 걸렸습니다\n')
 
 # numpy 방식
 start = time.time()
-ncounta = np.zeros(MAXNUM-1)
-for n in range(1,MAXNUM):
+ncounta = np.zeros(MAXNUM - 1, dtype=int)
+
+for n in range(1, MAXNUM):
     ncount = collatz(n)
-    ncounta[n-1] = ncount
+    ncounta[n - 1] = ncount
 
-
-# # 최대값, 평균, 중앙값, 표준편차, 최빈값,
+print(f'[NumPy 방식]')
 print(f'최대값={np.max(ncounta)}')
-print(f'평균= {np.mean(ncounta):.5f}')
+print(f'평균={np.mean(ncounta):.5f}')
 print(f'중앙값={np.median(ncounta)}')
-print(f'표준편차 ={np.std(ncounta):.5f}')
-# print(f'최빈값 = {np.bincount(ncounta).argmax():.5f}')
+print(f'표준편차={np.std(ncounta):.5f}')
+mode_val_np = np.bincount(ncounta).argmax()
+print(f'최빈값={mode_val_np}')
 end = time.time()
 print(f'{end - start:.5f} 초가 걸렸습니다')
